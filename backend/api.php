0fa602ef7aebfa1e618ea4f7b99ad68a993bad8c
Jose J. Pardines <info@josejpg.com>
2019-12-09 01:35:44 +0100

    Add API code.

---
+++ backend/api.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User: Jose J. Pardines
+ * Date: 2019-12-08
+ */
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+require_once('node.class.php');
+$node = new Node();
+$node->setDB($node->connect("localhost"));
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $node->getNodes($_GET);
+} else {
+    $node->setError('HTTP RQUEST not supported', 500);
+}
+echo $node;
